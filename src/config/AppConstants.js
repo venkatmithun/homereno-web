830a05a4968ec65b07d23ff98eada9dbf29f359d
venkatmithun <venkatmithun.k@gmail.com>
2021-04-30 16:52:52 -0700

    pages

--- src/config/AppConstants.js
+++ src/config/AppConstants.js
@@ -1,19 +1,19 @@
 //http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080
 export const AppConstants = {
   API_BASE_URL:
-    'http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/',
+    'https://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/',
   OAUTH2_URL:
-    'http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/',
+    'https://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/',
   API_URL:
-    'http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/api/',
+    'https://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/api/',
   AUTH_API:
-    'http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/api/auth/',
+    'https://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/api/auth/',
   GOOGLE_AUTH_URL:
-    'http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/google?redirect_uri=https://venkatmithun.github.io/login',
+    'https://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/google?redirect_uri=https://venkatmithun.github.io/login',
   FACEBOOK_AUTH_URL:
-    'http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/facebook?redirect_uri=https://venkatmithun.github.io/login',
+    'https://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/facebook?redirect_uri=https://venkatmithun.github.io/login',
   GITHUB_AUTH_URL:
-    'http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/github?redirect_uri=https://venkatmithun.github.io/login',
+    'https://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/github?redirect_uri=https://venkatmithun.github.io/login',
   LINKEDIN_AUTH_URL:
-    'http://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/linkedin?redirect_uri=https://venkatmithun.github.io/login',
+    'https://ec2-35-163-200-125.us-west-2.compute.amazonaws.com:8080/oauth2/authorization/linkedin?redirect_uri=https://venkatmithun.github.io/login',
 };
